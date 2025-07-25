f8b10947f0dc2a7087b026c919d5510ba8ab2e19
Pasha Sich <97337742+Kapidastra@users.noreply.github.com>
2025-07-25 14:50:55 +0200

    Added Solution 7

--- src/components/ErrorNotification.tsx
+++ src/components/ErrorNotification.tsx
@@ -7,10 +7,6 @@ interface Props {
 }
 
 export const ErrorNotification: React.FC<Props> = ({ message, onClose }) => {
-  // if (!message) {
-  //   return null;
-  // }
-
   return (
     <div
       data-cy="ErrorNotification"
